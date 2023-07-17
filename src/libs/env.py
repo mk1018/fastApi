616500bea1a162e259d7b8511971e04ee87d61e8
mk1018 <ktmn1018@yahoo.co.jp>
2023-07-17 12:30:10 +0900

    refactor: add anotetion.
    env.py内の関数にアノテーションを追加

--- src/libs/env.py
+++ src/libs/env.py
@@ -1,13 +1,13 @@
 import os
 
-def system_openai_api_key():
+def system_openai_api_key() -> str:
     return os.getenv("SYSTEM_OPENAI_API_KEY")
 
-def system_azure_openai_api_key():
+def system_azure_openai_api_key() -> str:
     return os.getenv("SYSTEM_AZURE_OPENAI_API_KEY")
 
-def system_claude_api_key():
+def system_claude_api_key() -> str:
     return os.getenv("SYSTEM_CLAUDE_API_KEY")
 
-def system_bird_api_key():
+def system_bird_api_key() -> str:
     return os.getenv("SYSTEM_BIRD_API_KEY")
