262b149bd861ce89c97c47d933253c04a85c6a95
mk1018 <ktmn1018@yahoo.co.jp>
2023-07-17 13:21:16 +0900

    fix: Changed indentation in __test.py from tabs to four spaces.
    タブをスペースに直しました。

--- src/tests/__test.py
+++ src/tests/__test.py
@@ -2,8 +2,8 @@ import os
 from functools import wraps
 
 def set_run_test(func):
-	@wraps(func)    
-	def wrapper(*args, **kwargs):
-		os.environ['RUN_TEST'] = "True"
-		return func(*args, **kwargs)
-	return wrapper
+    @wraps(func)
+    def wrapper(*args, **kwargs):
+        os.environ['RUN_TEST'] = "True"
+        return func(*args, **kwargs)
+    return wrapper
